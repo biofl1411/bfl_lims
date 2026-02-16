ea7ae7048c6bd9811ad79ac4803bb502591fb81a
biofl1411 <biofl1411@gmail.com>
2026-02-16 13:03:27 +0900

    초기 4개 파일 추가

---
+++ deploy.ps1
@@ -0,0 +1,7 @@
+﻿param($message = "Update")
+Write-Host " 배포 시작..." -ForegroundColor Cyan
+git add .
+git commit -m $message
+git push origin main
+Write-Host " 배포 완료!" -ForegroundColor Green
+Write-Host " https://biofl1411.github.io/bfl_lims/" -ForegroundColor Yellow
